04cc17f27a5d4e4a9421dc67262db76a48484c75
Derrick Nguyen <derrickhnguyen@outlook.com>
2016-07-19 12:02:50 -0700

    Moved app.locals.pretty to only development environment

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.locals.pretty = true;
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -38,6 +37,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  app.locals.pretty = true;
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
